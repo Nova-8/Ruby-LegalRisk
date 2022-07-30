c4595b8dfdf9b8c0bd8bbc8f171adb515dbb6e63
Matt Wolfe <m5wolfe@uwaterloo.ca>
2022-07-30 10:08:58 -0400

    update gemspec

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,9 @@ class HomeController < ApplicationController
   
   def index
     if params.has_key?(:query)
-      @num = Integer(params[:query]) rescue nil
-      unless @num.nil?
-        @res = EvenChecker.is_even?(@num)
+      @query = params[:query]
+      unless @query.nil?
+        @res = EvenChecker.is_even?(@query)
       end
     end
 
